2c5c709f15b26461ea2cd7acd2ba6ab6ba2eb73b
Mattheus2403 <matheusmoraidois@gmail.com>
2024-07-29 23:32:28 -0300

    feat: add project

---
+++ src/main/java/one/dio/ProjetoJavaRestfulApiSantanderBootcamp2024Application.java
@@ -0,0 +1,13 @@
+package one.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoJavaRestfulApiSantanderBootcamp2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoJavaRestfulApiSantanderBootcamp2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/one/dio/ProjetoJavaRestfulApiSantanderBootcamp2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package one.dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoJavaRestfulApiSantanderBootcamp2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
